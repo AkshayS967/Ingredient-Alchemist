ada5294b00d7e8080acb236c2901217a96a7fa5c
Akshay <akshay.gokulam1@gmail.com>
2024-01-04 03:18:53 +0530

    add ecosystem

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [{
+    script: 'npm start',
+  }],
+
+  deploy : {
+    production : {
+      key: 'FinalProjectServerKey.pem',
+      user : 'ubuntu',
+      host : '13.126.109.92',
+      ref  : 'origin/main',
+      repo : 'git@github.com:AkshayS967/Ingredient-Alchemist.git',
+      path : '/home/ubuntu/app/',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes',
+    }
+  }
+};
