f697292b60b71a706c92f4802b5567a2da8b8fa5
Akshay <akshay.gokulam1@gmail.com>
2024-01-04 23:12:45 +0530

    update ecosystem

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -10,11 +10,10 @@ module.exports = {
       host : '13.126.109.92',
       ref  : 'origin/main',
       repo : 'git@github.com:AkshayS967/Ingredient-Alchemist.git',
-      path : '/home/ubuntu/app/',
+      path : '/home/ubuntu/',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
-      'ssh_options': 'ForwardAgent=yes',
     }
   }
 };
