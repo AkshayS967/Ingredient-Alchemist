43881c22e7f944fa12d795a43ce183a690a586e3
Akshay <akshay.gokulam1@gmail.com>
2024-04-21 02:43:47 +0530

    Update gen ai to gpt2

--- backend/azure_server/gen_ai.py
+++ backend/azure_server/gen_ai.py
@@ -1,34 +1,21 @@
-from transformers import AutoModelForSeq2SeqLM
-from transformers import AutoTokenizer
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-model = "AkshayPM/ingredient_to_recipe"
-tokenizer = AutoTokenizer.from_pretrained(model)
-model = AutoModelForSeq2SeqLM.from_pretrained(model)
-
-generation_kwargs = {
-    "max_length": 512,
-    "min_length": 256,
-    "no_repeat_ngram_size": 3,
-    "do_sample": True,
-    "top_k": 60,
-    "top_p": 0.95
-}
-
+model = 'AkshayPM/gpt2_ingredient'
+tokenizer = GPT2Tokenizer.from_pretrained(model)
+model = GPT2LMHeadModel.from_pretrained(model)
 
 def generate_recipe(ingredients):
-    ingredients = "Ingredients: " + ", ".join('"'+ingredient+'"' for ingredient in ingredients)
-    print(ingredients)
-    inputs = tokenizer(
-        ingredients, 
-        max_length=512, 
-        padding="max_length",
-        truncation=True, 
-        return_tensors="pt"
+    ingredients = ", ".join('"'+ingredient+'"' for ingredient in ingredients)
+    prompt =  "Ingredients:\n" + ingredients + "\n\nRecipe:"
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+
+    gen_tokens = model.generate(
+        input_ids,
+        do_sample=True,
+        temperature=0.9,
+        max_length=512,
+        pad_token_id=tokenizer.pad_token_id,
     )
-
-    input_ids = inputs.input_ids
-    attention_mask = inputs.attention_mask
-    generate_ids = model.generate(input_ids=input_ids, attention_mask=attention_mask, **generation_kwargs)
-    return tokenizer.batch_decode(generate_ids, skip_special_tokens=True)[0]
-
+    gen_text = tokenizer.batch_decode(gen_tokens, skip_special_tokens=True)[0]
+    return gen_text
 
